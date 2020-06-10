dff0e0eb1da0ba3997711ef7c0b49c049c6045eb
kondo takashi <biguru124you@yahoo.co.jp>
2020-06-10 03:46:26 +0000

    課題修正

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -1,13 +1,50 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.profile')
 
-        <title>Myprofile</title>
-    </head>
-    <body>
-        <h1>プロフィール編集作成画面</h1>
-    </body>
-</html>
+
+@section('title', 'プロフィールの編集')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>My プロフィール編集</h2>
+                <form action="{{ action('Admin\ProfileController@update') }}" method="post" enctype="multipart/form-data">
+
+                @if (count($errors) > 0)
+                        <ul>
+                            @foreach($errors->all() as $e)
+                                <li>{{ $e }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+                    <div class="form-group row">
+                        <label class="col-md-2">name</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="title" value="{{ old('title') }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">gender</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" >
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">hobby</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="title" value="{{ old('title') }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">introduction</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="body" rows="20">{{ old('body') }}</textarea>
+                        </div>
+                    </div>
+                    {{ csrf_field() }}
+                    <input type="submit" class="btn btn-primary" value="更新">
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
