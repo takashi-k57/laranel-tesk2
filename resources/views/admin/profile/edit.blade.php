0e55148617e9c4495540c04762dacfa9e4d944a5
kondo takashi <biguru124you@yahoo.co.jp>
2020-06-13 04:18:02 +0000

    修正2

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -18,31 +18,36 @@
                         </ul>
                     @endif
                     <div class="form-group row">
-                        <label class="col-md-2">name</label>
+                        <label class="col-md-2"　for="name">name</label>
                         <div class="col-md-10">
-                            <input type="text" class="form-control" name="name" value="{{ old('name') }}">
+                            <input type="text" class="form-control" name="name" value="{{ $profiles_from->name }}">
                         </div>
                     </div>
                     <div class="form-group row">
-                        <label class="col-md-2">gender</label>
+                        <label class="col-md-2" for="gender">gender</label>
                         <div class="col-md-10">
-                            <input type="text" class="form-control" name="gender" value="{{ old('gender') }}" >
+                            <input type="text" class="form-control" name="gender" value="{{ $profiles_from->gender }}" >
                         </div>
                     </div>
                     <div class="form-group row">
-                        <label class="col-md-2">hobby</label>
+                        <label class="col-md-2" for="hobby">hobby</label>
                         <div class="col-md-10">
-                            <input type="text" class="form-control" name="hobby" value="{{ old('hobby') }}">
+                            <input type="text" class="form-control" name="hobby" value="{{ $profiles_from->hobby }}">
                         </div>
                     </div>
                     <div class="form-group row">
-                        <label class="col-md-2">introduction</label>
+                        <label class="col-md-2" for="introduction">introduction</label>
                         <div class="col-md-10">
-                            <textarea class="form-control" name="introduction" rows="20">{{ old('introduction') }}</textarea>
+                            <textarea class="form-control" name="introduction" rows="20">{{ $profiles_from->introduction }}</textarea>
                         </div>
                     </div>
-                    {{ csrf_field() }}
-                    <input type="submit" class="btn btn-primary" value="更新">
+                    <div class="form-group row">
+                        <div class="col-md-10">
+                            <input type="hidden" name="id" value="{{ $profiles_form->id }}">
+                            {{ csrf_field() }}
+                            <input type="submit" class="btn btn-primary" value="更新">
+                        </div>
+                    </div>
                 </form>
             </div>
         </div>
