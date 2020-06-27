b4349f7c54c034b37d2522e68d08575554f5ba0e
kondo takashi <biguru124you@yahoo.co.jp>
2020-06-27 03:49:50 +0000

    laravel課題8完了

---
+++ resources/views/profile/index.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.profile')
+
+@section('content')
+<div class="container">
+        <hr color="#c0c0c0">
+        @if (!is_null($headline))
+            <div class="row">
+                <div class="headline col-md-10 mx-auto">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="caption mx-auto">
+                                <div class="image">
+                                    @if ($headline->image_path)
+                                        <img src="{{ asset('storage/image/' . $headline->image_path) }}">
+                                    @endif
+                                </div>
+                                <div class="title p-2">
+                                    <h1>{{ str_limit($headline->name, 100) }}</h1>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <p class="body mx-auto">{{ str_limit($headline->gender, 650) }}</p>
+                            <dd>[趣味]</dd>
+                            <p class="body mx-auto">{{ str_limit($headline->hobby, 650) }}</p>
+                            <dd>[自己紹介]</dd>
+                            <p class="body mx-auto">{{ str_limit($headline->introduction, 650) }}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endif
+    </div>
+    </div>
+@endsection
